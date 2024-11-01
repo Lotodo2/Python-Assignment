e41af79096991edc52982314673629abba5150b0
Lotodo2 <aaronpkiach2@gmail.com>
2024-11-01 19:51:59 +0300

    Assignment 1

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
-name="My name is called Aaron Pkiach."
-age="My age is 27 "
-print(name,age)
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+print(f"Hello, {name}! You are {age} years old and live in {location}. Nice to meet you!")
